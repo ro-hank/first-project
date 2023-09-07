2cb11e0d550ec1c431f719afa28d38e0b8ba5a18
rohan <khakharr@cardiff.ac.uk>
2023-09-07 22:53:35 +0000

    parsing paremeters

--- quiz.py
+++ quiz.py
@@ -17,15 +17,14 @@ def main():
     
     num_questions = int(input("enter number of questions 1 - 10"))
     difficulty = int(input("1 - easy, 2 - medium, 3 - difficult"))
-    allAPI(difficulty)
+    allAPI(difficulty, num_questions)
 
     
     
 
-def allAPI(difficulty):
+def allAPI(difficulty, num_questions):
     main_api = 'https://opentdb.com/api.php?amount=1'
     json_data = requests.get(main_api).json()
-    print(json_data)
     response_code = json_data['response_code']
     question = json_data['results'][0]['question']
     correct_answer = json_data['results'][0]['correct_answer']
@@ -36,9 +35,11 @@ def allAPI(difficulty):
     print(f'\nresponse code: {response_code} \nquestion: {question}\ncorrect_answer: {correct_answer} \nincorrect_answers {incorrect_answers_str}\n')
     print(f'diff: {difficulty}')
 
+    game(json_data, num_questions)
 
-def game(num_questions):
-    exit() 
+def game(json_data, num_questions):
+    print(json_data)
+    print(num_questions)
 
 
 

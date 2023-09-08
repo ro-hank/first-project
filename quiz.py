16ef10cd04ed06be9d7da99f8819c1a705bb1025
rohan <khakharr@cardiff.ac.uk>
2023-09-08 12:37:26 +0000

    single question quiz and score working

--- quiz.py
+++ quiz.py
@@ -1,10 +1,11 @@
 import requests
+import random
 '''
 quiz project
 using apis
 scoreboard using database ?
 '''
-
+    #user chooses settings
 def main():
     print('''
           
@@ -21,25 +22,44 @@ def main():
 
     
     
+    #calls api
 
 def allAPI(difficulty, num_questions):
     main_api = 'https://opentdb.com/api.php?amount=1'
     json_data = requests.get(main_api).json()
+    print(f'diff: {difficulty}')
+    game(json_data, num_questions)
+
+
+def game(json_data, num_questions):
+    score = 0
+    count = 0
+    answers = []
+
     response_code = json_data['response_code']
     question = json_data['results'][0]['question']
     correct_answer = json_data['results'][0]['correct_answer']
-    incorrect_answers_str = json_data['results'][0]['incorrect_answers']
+    incorrect_answers_ls = json_data['results'][0]['incorrect_answers']
     
+    print(f'\nresponse code: {response_code} \nquestion: {question}\ncorrect_answer: {correct_answer} \nincorrect_answers {incorrect_answers_ls}\n')
+    answers.append(correct_answer)
+    for i in range(len(incorrect_answers_ls)):
+        answers.append(incorrect_answers_ls[i])
 
-    
-    print(f'\nresponse code: {response_code} \nquestion: {question}\ncorrect_answer: {correct_answer} \nincorrect_answers {incorrect_answers_str}\n')
-    print(f'diff: {difficulty}')
+    random.shuffle(answers)
+    print(answers)
 
-    game(json_data, num_questions)
 
-def game(json_data, num_questions):
-    print(json_data)
-    print(num_questions)
+    print("\nEnter number\n")
+    j = 1
+    for j, choice in enumerate(answers, 1):
+        print(j, choice)
+    user_choice = int(input())
+
+
+    if user_choice == (answers.index(correct_answer) + 1):
+        score = score + 1    
+    print(score)
 
 
 
